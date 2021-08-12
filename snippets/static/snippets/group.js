d12d3dfab2b3dc12eb3d7e83b4eb0609e229e0ce
Anthony-J-Garot <tone.garot@gmail.com>
2021-08-12 14:55:47 -0700

    Cleans JavaScript per ESLint

--- snippets/static/snippets/group.js
+++ snippets/static/snippets/group.js
@@ -1,21 +1,21 @@
 // Start the WebSocket connection
+/* global oTran */
 oTran.connectWS();
 
 // Clear button
-document.querySelector('#btn-clear-log').onclick = function (e) {
-    // Clear the log and anything that was typed so far in the input
-    document.querySelector('#json').value = '';
+document.querySelector('#btn-clear-log').onclick = () => {
+  // Clear the log and anything that was typed so far in the input
+  document.querySelector('#json').value = '';
 };
 
 // Disconnect button
-document.querySelector('#disconnect').onclick = function (e) {
-    console.log(oTran.webSocket);
-    if (oTran.webSocket.readyState === WebSocket.OPEN) {
-        oTran.webSocket.close();
-        //oTran.webSocket = null;
-        this.value = "Connect";
-    } else {
-        oTran.connectWS();
-        this.value = "Disconnect";
-    }
+document.querySelector('#disconnect').onclick = () => {
+  console.log(oTran.webSocket);
+  if (oTran.webSocket.readyState === WebSocket.OPEN) {
+    oTran.webSocket.close();
+    document.querySelector('#disconnect').value = 'Connect';
+  } else {
+    oTran.connectWS();
+    document.querySelector('#disconnect').value = 'Disconnect';
+  }
 };
--- snippets/static/snippets/transactions.js
+++ snippets/static/snippets/transactions.js
@@ -1,89 +1,93 @@
 // This is sort of like a class
 
-let oTran = new Object();
+const oTran = {};
+
 oTran.webSocket = null;
 //oTran.roomName = JSON.parse(document.getElementById('room-name').textContent);
 
 // Perform the WS connection
 oTran.connectWS = function () {
 
-    const ws_scheme = window.location.protocol == "https:" ? "wss://" : "ws://";
-    const endpoint = '/graphql/';
+  const ws_scheme = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+  const endpoint = '/graphql/';
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
-    this.webSocket = new WebSocket(
-        ws_scheme
-        + window.location.host
-        + endpoint, 'graphql-ws'
-    );
-    console.log("host is " + window.location.host);
-    console.log("url is " + this.webSocket.url);
-    console.log("protocol is " + this.webSocket.protocol);
+  // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
+  this.webSocket = new WebSocket(
+    ws_scheme
+    + window.location.host
+    + endpoint, 'graphql-ws'
+  );
+  console.log('host is ' + window.location.host);
+  console.log('url is ' + this.webSocket.url);
+  console.log('protocol is ' + this.webSocket.protocol);
 
-    this.webSocket.onmessage = oTran.onmessage;
-    this.webSocket.onopen = oTran.onopen;
-    this.webSocket.onclose = oTran.onclose;
-    this.webSocket.onerror = oTran.error;
-}
+  this.webSocket.onmessage = oTran.onmessage;
+  this.webSocket.onopen = oTran.onopen;
+  this.webSocket.onclose = oTran.onclose;
+  this.webSocket.onerror = oTran.error;
+};
 
 // onMessage event
 // This occurs when the Websocket sends back data.
 oTran.onmessage = function (e) {
-    const data = JSON.parse(e.data);
-    console.log(data); // Show the whole data package for debugging
 
-    // Make sure we are who we think we are . . . or something
-    console.log("op_id: " + data.id);
+  const data = JSON.parse(e.data);
+  console.log(data); // Show the whole data package for debugging
 
-    switch (data.type) {
-        // YYZ - ANTHONY - Who set the data type to data? Did I?
-        case 'data':
-            // If there are errors, let me know right away
-            if (data.payload.errors) {
-                for (const error of data.payload.errors) {
-                    console.log(error.message);
-                }
-                return;
-            }
+  // Make sure we are who we think we are . . . or something
+  console.log('op_id: ' + data.id);
 
-            // Valid payload?
-            if (data.payload.data) {
-                // Shorthand for the useful payload
-                let payload = data.payload.data.onSnippetNoGroup;
+  switch (data.type) {
+    // YYZ - ANTHONY - Who set the data type to data? Did I?
+    case 'data':
+      // If there are errors, let me know right away
+      if (data.payload.errors) {
+        for (const error of data.payload.errors) {
+          console.log(error.message);
+        }
+        return;
+      }
 
-                console.log("sender: " + payload.sender);
-                console.log("ok?: " + payload.ok);
-                console.log("transType: " + payload.transType);
+      // Valid payload?
+      if (data.payload.data) {
 
-                // Access the database object
-                let snippet = payload.snippet;
-                console.log(snippet);
-                document.querySelector('#json').value = "" +
-                    "Transaction Type: " + payload.transType + "\n" +
-                    "Snippet:\n" +
-                    JSON.stringify(payload.snippet, null, 2);
-            } else {
-                console.log("Invalid payload");
-                return;
-            }
+        // Shorthand
+        /** @namespace data.onSnippetNoGroup */
+        const payload = data.payload.data.onSnippetNoGroup;
 
-            break;
-        default:
-            console.log("Unknown data.type [" + data.type + "]");
-    }
+        /** @namespace payload.transType */
+        console.log('transType: ' + payload.transType);
+        console.log('sender: ' + payload.sender);
+        console.log('ok?: ' + payload.ok);
+
+        // Access the database object
+        /** @namespace payload.snippet */
+        document.querySelector('#json').value = '' +
+          'Transaction Type: ' + payload.transType + '\n' +
+          'Snippet:\n' +
+          JSON.stringify(payload.snippet, null, 2);
+      } else {
+        console.log('Invalid payload');
+        return;
+      }
+
+      break;
+    default:
+      console.log('Unknown data.type [' + data.type + ']');
+  }
 };
 
 // onOpen event
 oTran.onopen = function open() {
-    console.log('Chat socket opened');
+  console.log('Chat socket opened');
 
-    // See channels_graphql_ws/graphql_ws_consumer.py for what is wanted
-    // to start a subscription. Can also look at the unit test.
-    let content = new Object();
-    content.id = 1; // A unique sequence? See channels_graphql_ws/graphql_ws_consumer.py:315
-    content.type = 'start';
-    let payload = new Object();
-    payload.query = String.raw`
+  // See channels_graphql_ws/graphql_ws_consumer.py for what is wanted
+  // to start a subscription. Can also look at the unit test.
+  let content = {};
+  content.id = 1; // A unique sequence? See channels_graphql_ws/graphql_ws_consumer.py:315
+  content.type = 'start';
+  let payload = {};
+  payload.query = String.raw`
 subscription subNoGroup {
   onSnippetNoGroup {
     sender
@@ -99,35 +103,37 @@ subscription subNoGroup {
   }
 }
     `;
-    payload.operationName = 'subNoGroup';
-    content.payload = payload;
+  payload.operationName = 'subNoGroup';
+  content.payload = payload;
 
-    let contentJSON = JSON.stringify(content, null, 2);
+  let contentJSON = JSON.stringify(content, null, 2);
 
-    // This looks good in the console or in a <textarea>, but it's no longer JSON.
-    contentPrettyJSON = textAreaCleanup(contentJSON);
-    console.log("Subscription JSON " + contentPrettyJSON);
-    document.querySelector('#json').value = contentPrettyJSON;
+  // This looks good in the console or in a <textarea>, but it's no longer JSON.
+  const contentPrettyJSON = textAreaCleanup(contentJSON);
+  console.log('Subscription JSON ' + contentPrettyJSON);
+  document.querySelector('#json').value = contentPrettyJSON;
 
-    // Send the JSON to API via the WebSocket
-    oTran.webSocket.send(contentJSON);
+  // Send the JSON to API via the WebSocket
+  oTran.webSocket.send(contentJSON);
 };
 
 // onClose event
 oTran.onclose = function (closeEvent) {
-    // console.log(closeEvent);
-    if (closeEvent.wasClean) {
-        console.log('Chat socket closed, clean close. Code [' + closeEvent.code + ']');
-    } else {
-        console.error('Chat socket closed, unclean close', closeEvent);
-    }
+  // console.log(closeEvent);
+  if (closeEvent.wasClean) {
+    console.log('Chat socket closed, clean close. Code [' + closeEvent.code + ']');
+  } else {
+    console.error('Chat socket closed, unclean close', closeEvent);
+  }
 };
 
 // onError event
 oTran.onerror = function (event) {
-    console.error('Websocket error observed: ', event);
+  console.error('Websocket error observed: ', event);
 };
 
 function textAreaCleanup(str) {
-    return str.replace(/\\n/g, "\r\n");
+  return str.replace(/\\n/g, '\r\n');
 }
+
+//export default oTran;
