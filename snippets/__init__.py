68d80c0f33485cabd27dfa36b578a78b34a81992
Anthony-J-Garot <tone.garot@gmail.com>
2021-09-08 11:31:44 -0700

    Moves whomai to __init__.py

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -1,10 +1,12 @@
-"""
-I was thinking it would be nice to capture GraphQL used in the App here.
-I may decide to put them into a separate file, or keep separate from unit tests.
-"""
+from django.conf import settings
 
 
-VERIFY_JWT_TOKEN_MUTATION = '''
+def whoami(info):
+    """
+Determines who the user is based upon the passed header token.
+    """
+
+    query = '''
 mutation mutVerifyJWT($token: String!) {
   verifyToken(token: $token) {
     payload
@@ -12,3 +14,26 @@ mutation mutVerifyJWT($token: String!) {
 }
 '''
 
+    # The token comes in through the headers prefaced with JWT
+    username = info.context.user  # default value; could be AnonymousUser
+    auth_string = info.context.META['HTTP_AUTHORIZATION']
+    if auth_string.startswith('JWT '):
+        print(f"Splitting auth_string [{auth_string}]")
+        jwt, token = auth_string.split(' ')
+
+        # Pass the token back to get the authenticated username.
+        # Note the import within the function else bad things happen:
+        #       raise AppRegistryNotReady("Apps aren't loaded yet.")
+        from mysite import schema
+        result = schema.schema.execute(
+            query,
+            context=info.context,
+            variables={"token": token}
+        )
+        username = result.data['verifyToken']['payload']['username']
+        if settings.DEBUG:
+            print(f"Username from mutVerifyJWT [{username}]")
+            print(f"Username from info.context.user [{info.context.user}]")
+            print("")
+
+    return username
