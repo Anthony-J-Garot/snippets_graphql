c2af83c19b9bfc1bca38b4ad17fd48227c88bb97
Anthony-J-Garot <tone.garot@gmail.com>
2021-09-10 08:29:32 -0700

    Fixes whoami() to not fail when no header given

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -4,6 +4,7 @@ from django.conf import settings
 def whoami(info):
     """
 Determines who the user is based upon the passed header token.
+If there is no HTTP_AUTHORIZATION header, default to back end user value.
     """
 
     query = '''
@@ -14,26 +15,30 @@ mutation mutVerifyJWT($token: String!) {
 }
 '''
 
-    # The token comes in through the headers prefaced with JWT
-    username = info.context.user  # default value; could be AnonymousUser
-    auth_string = info.context.META['HTTP_AUTHORIZATION']
-    if auth_string.startswith('JWT '):
-        print(f"Splitting auth_string [{auth_string}]")
-        jwt, token = auth_string.split(' ')
+    # default value from the back end; could be AnonymousUser
+    username = info.context.user
 
-        # Pass the token back to get the authenticated username.
-        # Note the import within the function else bad things happen:
-        #       raise AppRegistryNotReady("Apps aren't loaded yet.")
-        from mysite import schema
-        result = schema.schema.execute(
-            query,
-            context=info.context,
-            variables={"token": token}
-        )
-        username = result.data['verifyToken']['payload']['username']
-        if settings.DEBUG:
-            print(f"whoami(): Username from mutVerifyJWT [{username}]")
-            print(f"whoami(): Username from info.context.user [{info.context.user}]")
-            print("")
+    # The token comes in through the headers prefaced with JWT
+    if 'HTTP_AUTHORIZATION' in info.context.META:
+        auth_string = info.context.META['HTTP_AUTHORIZATION']
+
+        if auth_string.startswith('JWT '):
+            print(f"Splitting auth_string [{auth_string}]")
+            jwt, token = auth_string.split(' ')
+
+            # Pass the token back to get the authenticated username.
+            # Note the import within the function else bad things happen:
+            #       raise AppRegistryNotReady("Apps aren't loaded yet.")
+            from mysite import schema
+            result = schema.schema.execute(
+                query,
+                context=info.context,
+                variables={"token": token}
+            )
+            username = result.data['verifyToken']['payload']['username']
+            if settings.DEBUG:
+                print(f"whoami(): Username from mutVerifyJWT [{username}]")
+                print(f"whoami(): Username from info.context.user [{info.context.user}]")
+                print("")
 
     return username
